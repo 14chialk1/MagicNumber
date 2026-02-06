95541196c0e8c73d1eadfa589c5a8a54cecb0a84
14chialk1 <30090534@cc.peralta.edu>
2026-02-06 13:08:59 -0800

    Refactor MagicNumber to simplify calculations

--- MagicNumber.cpp
+++ MagicNumber.cpp
@@ -5,9 +5,9 @@ int main() {
     int favourite_integer;
     cout << "Write down your favourite integer!"<< endl;
     cin >> favourite_integer;
-    cout << "Multiplying your favourite integer by 2 gives " << 2*favourite_integer << endl;
-    cout << "Adding 10 to that gives " << 2*favourite_integer+10 << endl;
-    cout << "Further dividing by 2 gives " << (2*favourite_integer+10)/2 << endl;
-    cout << "Lastly, subtracting the favourite integer gives " << ((2*favourite_integer+10)/2)-favourite_integer << endl;
+    magic_performed_favourite_integer = ((2*favourite_integer+10)/2)-favourite_integer
+    cout << "Nutters! The favourite integer is  " << magic_performed_favourite_integer << endl;
     return 0;
 }
+
+
